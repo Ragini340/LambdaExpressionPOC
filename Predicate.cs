55bee08730dee5b9f4a2378f191680c606346339
Ragini340 <ragini31bxr@gmail.com>
2022-11-30 20:19:12 +0530

    Predicate

---
+++ Predicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionPOC
+{
+	class Predicate
+	{
+		public static void Main()
+		{
+			Predicate<string> isUpper = IsUpperCase;
+
+			bool result = isUpper("hello world!!");
+
+			Console.WriteLine(result);
+		}
+
+		public static bool IsUpperCase(string str)
+		{
+			return str.Equals(str.ToUpper());
+		}
+
+	}
+}
